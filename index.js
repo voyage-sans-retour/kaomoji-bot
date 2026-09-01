70f349731bfadfdba5dde3ecd1ffb1aee2c54b7e
Seti <setimiti@gmail.com>
2026-09-01 17:49:57 -0400

    add help command

--- index.js
+++ index.js
@@ -12,10 +12,43 @@ app.command("/kaomoji-bot-ping", async ({ command, ack, respond }) => {
   const start = Date.now();
   await ack();
   const latency = Date.now() - start;
-  await respond({ text: `Pong!\nLatency: ${latency}ms` });
+  await respond({ text: `Hi, I'm a bot that will give you list of kaomojis you can copy+paste for use in your messages. Type /kaomoji-bot-help to see a directory of emotions.\nLatency: ${latency}ms` });
 });
 
 (async () => {
   await app.start();
   console.log("bot is running!");
 })();
+
+app.command("/kaomoji-bot-help", async ({ ack, respond }) => {
+  await ack();
+  await respond({
+    text:
+`Available Commands:
+/kaomoji-bot-ping - Check bot latency
+/kaomoji-bot-help - Get a directory of available commands\n
+
+Emoticons:
+/kaomoji-bot-happy 
+/kaomoji-bot-sad
+/kaomoji-bot-angry
+/kaomoji-bot-tired 
+/kaomoji-bot-embarrassed
+/kaomoji-bot-sympathetic
+/kaomoji-bot-annoyed
+/kaomoji-bot-afraid
+/kaomoji-bot-pain
+/kaomoji-bot-confused
+/kaomoji-bot-surprised
+/kaomoji-bot-love
+
+Actions:
+/kaomoji-bot-greeting
+/kaomoji-bot-winking
+/kaomoji-bot-hugging
+/kaomoji-bot-writing
+/kaomoji-bot-running
+/kaomoji-bot-sleeping
+`
+  });
+});
